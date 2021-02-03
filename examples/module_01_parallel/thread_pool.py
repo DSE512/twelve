5849516d7c56531686617dca11ddac95f935fdbd
yngtodd <young.todd.mk@gmail.com>
2021-02-02 23:40:13 -0500

    Clean up thread pool example

--- examples/module_01_parallel/thread_pool.py
+++ examples/module_01_parallel/thread_pool.py
@@ -1,25 +1,33 @@
 import time
 
-from concurrent.futures import ThreadPoolExecutor
+from concurrent import futures
 
 
-def snooze():
-    print("Sleeping for two seconds.")
-    time.sleep(2)
-    print("Done sleeping.")
-    return "I am awake!"
+def snooze(seconds):
+    """Snooze for seconds
+
+    Args:
+        seconds: amount of time to snooze for
+
+    Returns:
+        seconds: So that we can see the order
+                 in which threads finish
+    """
+    print(f"Sleeping for {seconds} seconds.")
+    time.sleep(seconds)
+    return f" Slept for {seconds} seconds"
 
 
 def main():
     start = time.perf_counter()
 
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(snooze) for _ in range(4)]
+    with futures.ThreadPoolExecutor() as executor:
+        seconds = [3, 4, 1, 2]
+        futs = [executor.submit(snooze, sec) for sec in seconds]
 
     print(f"\nResults from executor:")
-    for fut in futures:
-        result = fut.result()
-        print(result)
+    for fut in futures.as_completed(futs):
+        print(fut.result())
 
     end = time.perf_counter()
     print(f"Total time: {end-start} seconds.")

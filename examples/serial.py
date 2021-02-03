142c7ae78a2a341064680290a7f66c6d9c4651d7
yngtodd <young.todd.mk@gmail.com>
2021-02-02 20:53:28 -0500

    Add threading example

---
+++ examples/serial.py
@@ -0,0 +1,23 @@
+import time
+
+
+def snooze():
+    print("Sleeping for two seconds.")
+    time.sleep(2)
+    print("Done sleeping.")
+
+
+def main():
+    start = time.perf_counter()
+
+    threads = []
+    for _ in range(4):
+        snooze()
+
+    end = time.perf_counter()
+    print(f"Total time: {end-start} seconds.")
+
+
+if __name__=="__main__":
+    main()
+

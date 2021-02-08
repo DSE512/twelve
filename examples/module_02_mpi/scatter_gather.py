4016524f402a556a415a3d150b92c2f2ff144f66
yngtodd <young.todd.mk@gmail.com>
2021-02-08 00:10:50 -0500

    Clean up prints

--- examples/module_02_mpi/scatter_gather.py
+++ examples/module_02_mpi/scatter_gather.py
@@ -1,7 +1,9 @@
 """
 scatter_pather.py
 
-Scatter values to all ranks and gather them back!
+Scatter values to all ranks and gather them back! Note that
+the number of values to be scattered equal the number of ranks
+that you are scattering to.
 """
 import numpy as np
 
@@ -11,16 +13,21 @@ from mpi4py import MPI
 def banner(comm=MPI.COMM_WORLD):
     """Print a banner of run info"""
     if comm.rank == 0:
+        print("="*18)
         print(f"Running on {comm.size} ranks")
+        print("="*18)
 
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.rank
+    size = comm.size
+
+    banner()
 
     if rank == 0:
-        data = [x for x in range(comm.size)]
-        print(f"Rank {rank} scattering {data}")
+        data = [x for x in range(size)]
+        print(f"\nRank {rank} scattering {data}")
     else:
         data = None
 
@@ -30,10 +37,10 @@ def main():
     comm.Barrier()
 
     if rank == 0:
-        print(f"****Gathering!****")
+        print(f"\n****Gathering!****\n")
 
     data = comm.gather(data, root=0)
-    print(f"Rank {rank} after gather has {data}")
+    print(f"Rank {rank} after gather has {data}\n")
 
 
 if __name__=="__main__":

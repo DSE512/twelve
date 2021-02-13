760ef59825470b3eba1a6245f2443287156fce46
Young, Todd <youngmt1@ornl.gov>
2021-02-13 17:00:23 -0500

    Add a bit of documentation

--- examples/module_01_parallel/multithread_images.py
+++ examples/module_01_parallel/multithread_images.py
@@ -3,13 +3,21 @@ from twelve.data import Images
 from concurrent import futures
 
 
-class MultiImages(Images):
+class MultiThreadImages(Images):
+    """Download image data using multiple threads
+
+    Most of the work here is done by
+    `twelve.data.image.Images`, where `Images` loops
+    over a list of urls pointing to images on unsplash,
+    downloading them one at a time. Here we overwrite
+    `Images.download` in order to use multiple threads.
+    This way we can speed up this I/O problem!
+    """
 
     def __init__(self, root="img"):
         super().__init__(root=root)
 
     def download(self):
-        # map over urls and download in parallel
         self.root.mkdir()
 
         print("Downloading...")
@@ -21,9 +29,8 @@ class MultiImages(Images):
 
 
 def main():
-    print("I am good")
-    images = MultiImages("data/img")
-    print("done")
+    # Note: if data/img already exists, this will do nothing!
+    images = MultiThreadImages("data/img")
 
 
 if __name__=="__main__":

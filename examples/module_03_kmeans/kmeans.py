ae95dfc6d3dcd5155531f1fb18f5a302937843c2
yngtodd <young.todd.mk@gmail.com>
2021-03-07 11:53:12 -0500

    Reformat
    fixing style

--- examples/module_03_kmeans/kmeans.py
+++ examples/module_03_kmeans/kmeans.py
@@ -8,18 +8,18 @@ from twelve.unsupervised.kmeans import Kmeans, kmeans_save
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--num_samples', type=int, default=100,
-                        help='dataset size')
-    parser.add_argument('--k', type=int, default=3,
-                        help='number of clusters of k-means algoritm')
-    parser.add_argument('--num_features', type=int, default=2,
-                        help='number of clusters of k-means algoritm')
-    parser.add_argument('--num_centers', type=int, default=3,
-                        help='number of actual clusters in the data')
-    parser.add_argument('--savepath', type=str, default="./results",
-                        help='path to the save loss curves')
-    parser.add_argument('--verbose', action="store_true",
-                        help='print what is going on')
+    parser.add_argument("--num_samples", type=int, default=100, 
+                        help="dataset size")
+    parser.add_argument("--k", type=int, default=3, 
+                        help="number of clusters of k-means algoritm")
+    parser.add_argument("--num_features", type=int, default=2,
+                        help="number of clusters of k-means algoritm")
+    parser.add_argument("--num_centers", type=int, default=3,
+                        help="number of actual clusters in the data")
+    parser.add_argument("--savepath", type=str, default="./results", 
+                        help="path to the save loss curves")
+    parser.add_argument("--verbose", action="store_true", 
+                        help="print what is going on")
     return parser.parse_args()
 
 
@@ -38,9 +38,9 @@ def main():
 
     if rank == 0:
         x_train, _ = datasets.make_blobs(
-            n_samples=args.num_samples, 
-            centers=args.num_centers, 
-            n_features=args.num_features
+            n_samples=args.num_samples,
+            centers=args.num_centers,
+            n_features=args.num_features,
         )
 
         subset_size = args.num_samples // size
@@ -56,5 +56,5 @@ def main():
         log(f"Received data of shape shape {subset.shape}", rank)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()

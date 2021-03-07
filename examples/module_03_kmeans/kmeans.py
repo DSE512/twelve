a07cec6fd32af52916bfe3601497f618c6697774
yngtodd <young.todd.mk@gmail.com>
2021-03-07 12:42:09 -0500

    Add save directory

--- examples/module_03_kmeans/kmeans.py
+++ examples/module_03_kmeans/kmeans.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from mpi4py import MPI
+from pathlib import Path
 from sklearn import datasets
 from argparse import ArgumentParser
 from twelve.unsupervised.kmeans import Kmeans, kmeans_save
@@ -37,23 +38,38 @@ def main():
     args = parse_args()
 
     if rank == 0:
-        x_train, _ = datasets.make_blobs(
+        x_train, y_train = datasets.make_blobs(
             n_samples=args.num_samples,
             centers=args.num_centers,
             n_features=args.num_features,
         )
 
         subset_size = args.num_samples // size
-        subset = np.zeros((subset_size, args.num_features), dtype=np.float64)
+        x_subset = np.zeros((subset_size, args.num_features), dtype=np.float64)
+        y_subset = np.zeros((subset_size,), dtype=np.float64)
     else:
         subset_size = args.num_samples // size
         x_train = np.zeros((args.num_samples, args.num_features), dtype=np.float64)
-        subset = np.zeros((subset_size, args.num_features), dtype=np.float64)
+        y_train = np.zeros((args.num_samples, args.num_features), dtype=np.float64)
+        x_subset = np.zeros((subset_size, args.num_features), dtype=np.float64)
+        y_subset = np.zeros((subset_size,), dtype=np.float64)
 
-    comm.Scatter([x_train, MPI.DOUBLE], subset, root=0)
+    comm.Scatter([x_train, MPI.DOUBLE], x_subset, root=0)
+    comm.Scatter([y_train, MPI.INT], y_subset, root=0)
 
     if args.verbose:
-        log(f"Received data of shape shape {subset.shape}", rank)
+        log(f"Received data, labels of shape {x_subset.shape}, {y_subset.shape}", rank)
+
+    model = Kmeans(args.k)
+    predictions, centroids = model.predict(x_subset)
+
+    kmeans_save(
+        data=x_subset,
+        target=y_subset,
+        predictions=predictions,
+        centroids=centroids,
+        path=Path(args.savepath).joinpath(f'save_rank{rank}')
+    )
 
 
 if __name__ == "__main__":

567f72ae11edb221d13c040389b571adc59b1b14
yngtodd <young.todd.mk@gmail.com>
2021-03-08 16:36:03 -0500

    Add Amdahl's Law

---
+++ twelve/profile/amdahl.py
@@ -0,0 +1,15 @@
+def amdahls_law(p, s):
+    """Speedup relative to proportion parallel
+
+    Amdahl's Law gives an idealized speed up we 
+    can expect for an algorithm given the proportion
+    that algorithm can be parallelized and the speed
+    we gain from that parallelization. The best case
+    scenario is that the speed up, `s`, is equal to
+    the number of processors available.
+
+    Args:
+        p: proportion parallel
+        s: speed up for the parallelized proportion
+    """
+    return 1 / ((1-p) + p/s)

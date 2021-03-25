b9e4de15086ff7ca9a5eff834f0fbdedf19b4e7b
yngtodd <young.todd.mk@gmail.com>
2021-03-25 09:47:52 -0400

    Add class example

---
+++ examples/module_04_measure/numba/classes.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from numba import jitclass
+from numba import int32, float32
+
+
+spec = [
+    ('value', int32),
+    ('array', float32[:]),
+]
+
+
+@jitclass(spec)
+class Bag(object):
+    def __init__(self, value):
+        self.value = value
+        self.array = np.zeros(value, dtype=np.float32)
+
+    @property
+    def size(self):
+        return self.array.size
+
+    def increment(self, val):
+        for i in range(self.size):
+            self.array[i] += val
+        return self.array
+
+    @staticmethod
+    def add(x, y):
+        return x + y
